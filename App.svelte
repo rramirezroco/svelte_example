0974609b2e97f6449b9f6bae6fe5834352aa2adc
Rosa Ramirez Roco <45204121+rramirezroco@users.noreply.github.com>
2022-10-16 00:00:21 -0300

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,8 @@
+<script>
+  import Form from "./Form.svelte";
+</script>
+  
+<main>
+	 <h2>Películas</h2>
+   <Form />
+</main>
---
+++ Form.svelte
@@ -0,0 +1,23 @@
+<script>
+  const movies = [
+    {
+      name: "Avengers",
+      available: 3,
+      quantity: 0
+    },
+    {
+      name: "Terminator",
+      available: 5,
+      quantity: 0
+    }
+  ];
+</script>
+
+{#each movies as movie} 
+<form>
+	<h3>{movie.name}</h3>
+	<button type="button" on:click={()=>movie.quantity -= 1} disabled={movie.quantity <=0}>-</button>
+	{movie.quantity}
+	<button type="button" on:click={()=>movie.quantity += 1} disabled={movie.quantity >= movie.available}>+</button>
+</form> 
+{/each}
